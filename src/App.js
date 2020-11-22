4ccc0083227f2793b4db80dc7b2e479a65d782bc
jsan <ahsan.wtc@gmail.com>
2020-11-22 11:49:22 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Grid, AppBar, Typography, Toolbar, Card, CardContent } from '@material-ui/core';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 function App() {
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark'
+    }
+  });
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Grid container direction="column">
+        <Grid item><AppBar position='static'><Toolbar><Typography className={classes.title}>Package Calculator</Typography></Toolbar></AppBar></Grid>
+        <Grid item container>
+          <Grid item xs={false} sm={2}></Grid>
+          <Grid item xs={12} sm={8}>
+          <Typography color="textSecondary" gutterBottom>Word of the Day</Typography>
+            <Card>
+              <CardContent>
+                
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={false} sm={2}></Grid>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
