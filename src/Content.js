9a9124e047039279542187f9dd4f6ed8c1b3658b
jsan <ahsan.wtc@gmail.com>
2020-12-12 14:42:25 +0100

    removing unused code

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Grid, TextField, Card, CardContent, Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { blue } from '@material-ui/core/colors';
 
 import { Application } from './store';
@@ -21,29 +21,12 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const Content = props => {
-  // const dispatch = useDispatch();
+const Content = () => {
   const classes = useStyles();
-  // const { 
-  //   days, albums, usbs, drones, /*videoCameras, photoCameras,*/ onInputChange, traveling
-  // } = props;
-
   const packageDays = useSelector(Application.Selectors.getPackageDays);
   const travelling = useSelector(Application.Selectors.getExpense(EXPENSES.travelling));
   const videoCameras = useSelector(Application.Selectors.getExpense(EXPENSES.videoCamera));
   const photoCameras = useSelector(Application.Selectors.getExpense(EXPENSES.photoCamera));
-
-
-  
-  // const travelingFields = traveling.map((_, i) => {
-  //   const label = `Traveling Cost on day ${i + 1}`;
-  //   const name = `traveling-${i}`;
-  //   return (
-  //     <Grid item xs={12} sm={4} key={`traveling-${i}`}>
-  //       <TextField variant="outlined" label={label} fullWidth value={traveling[i]} name={name} onChange={onInputChange} onFocus={onFocus}/>
-  //     </Grid>
-  //   );
-  // });
   
   return (
     <form className={classes.form} autoComplete="off">
@@ -53,30 +36,6 @@ const Content = props => {
         <ItemCard title="Videocameras cost" item={videoCameras} />
         <ItemCard title="Photocameras cost" item={photoCameras} />
         <ItemCard deliverable title="Deliverables" />
-        
-        {/* <Grid item xs={12}>
-          <TextField variant="outlined" label="Number of Days" fullWidth value={days} name="number-days" onChange={onInputChange} onFocus={onFocus}/>
-        </Grid>
-
-        <Grid item xs={12} sm={4}>
-          <TextField variant="outlined" label="Number of Days" fullWidth value={days} name="number-days" onChange={onInputChange} onFocus={onFocus}/>
-        </Grid>
-        {travelingFields} */}
-        {/* <Grid item xs={12} sm={4}>          
-          <TextField variant="outlined" label="Number of Albums" fullWidth value={albums} name="number-albums" onChange={onInputChange} onFocus={onFocus}/>
-        </Grid>
-        <Grid item xs={12} sm={4}>          
-          <TextField variant="outlined" label="Number of USBs" fullWidth value={usbs} name="number-usbs" onChange={onInputChange} onFocus={onFocus}/>
-        </Grid> */}
-        {/* <Grid item xs={12} sm={4}>          
-          <TextField variant="outlined" label="Number of Drones" fullWidth value={drones} name="number-drones" onChange={onInputChange} onFocus={onFocus}/>
-        </Grid> */}
-        {/* <Grid item xs={12} sm={4}>          
-          <TextField variant="outlined" label="Number of Video Cameras" fullWidth value={videoCameras} name="number-videoCameras" onChange={onInputChange} onFocus={onFocus}/>
-        </Grid> */}
-        {/* <Grid item xs={12} sm={4}>          
-          <TextField variant="outlined" label="Number of Photo Cameras" fullWidth value={photoCameras} name="number-photoCameras" onChange={onInputChange} onFocus={onFocus}/>
-        </Grid> */}
       </Grid>
     </form>
   );
